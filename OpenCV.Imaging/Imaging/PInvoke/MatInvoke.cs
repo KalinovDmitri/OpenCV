7b0f4320ddb7cf61f3f306f1eef8c6360c4f73c9
Калинов Дмитрий Владимирович <kadmvl@yandex.ru>
2015-11-26 18:12:15 +0400

    Last commit

---
+++ OpenCV.Imaging/Imaging/PInvoke/MatInvoke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using OpenCV.Core;
+
+namespace OpenCV.Imaging
+{
+    internal static class MatInvoke
+    {
+        #region Mat interaction
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr cveMatCreate();
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cveMatCreateData(IntPtr mat, int rows, int cols, int type);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr cveMatGetDataPointer(IntPtr mat);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr cveMatGetStep(IntPtr mat);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr cveInputArrayFromMat(IntPtr mat);
+
+        [DllImport(CvInvoke.ExternLibrary, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void cveMatRelease(ref IntPtr mat);
+        #endregion
+    }
+}
